e1d872a2344b7c945d243e72ca45905aaa7b26e3
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-07-19 12:19:53 +0300

    creates root Component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Feedback from './Feedback';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Feedback />
+      </>
+    );
+  }
+}
+
+export default App;
