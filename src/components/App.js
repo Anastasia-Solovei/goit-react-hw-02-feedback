117fa0786a1f549c0cbbed2727302f1abf6005d8
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-07-24 00:57:01 +0300

    creates class App with state and methods, rendering componets FeedbackOptions and Statistics

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
-import Feedback from './Feedback';
+
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+
+import { FEEDBACK_OPTIONS } from '../data/constants';
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = ({ target }) => {
+    const feedback = target.name.toLowerCase();
+
+    this.setState(prevState => {
+      return {
+        [feedback]: prevState[feedback] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
   render() {
+    const onLeaveFeedback = this.onLeaveFeedback;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
     return (
       <>
-        <Feedback />
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={FEEDBACK_OPTIONS}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
       </>
     );
   }
